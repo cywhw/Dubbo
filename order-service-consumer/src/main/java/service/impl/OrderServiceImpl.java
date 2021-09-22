c432dc230766de52a9e7a314c8a00be449f7afed
cywhw <Email>
2021-09-22 16:39:47 +0800

    修改1

---
+++ order-service-consumer/src/main/java/service/impl/OrderServiceImpl.java
@@ -0,0 +1,10 @@
+package service.impl;
+
+import service.OrderService;
+
+public class OrderServiceImpl implements OrderService {
+    @Override
+    public void initOrder(String userId) {
+        //查询用户的收货地址
+    }
+}
